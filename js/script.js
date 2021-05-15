0b0a2e805a95a45484e67c1121b66ad7fd995cd3
Ruan <karlosruan9@gmail.com>
2021-05-15 19:32:50 -0300

    paginacao, navegacao, botoes

---
+++ js/script.js
@@ -0,0 +1,7 @@
+
+function teste() {
+    var elems = document.getElementById('botão');
+            var instances = M.FloatingActionButton.init(elems, {
+              direction: 'top'
+            }); 
+}
