7b6dbbade437064e0aa7af43a74d99d1e1d10df9
Ruan <karlosruan9@gmail.com>
2021-05-30 14:24:40 -0300

    trabalhando com select

--- js/script.js
+++ js/script.js
@@ -57,3 +57,41 @@ $(document).ready(function(){
 $(document).ready(function(){
   $('select').formSelect();
 });
+
+//datepicker
+
+/*$(document).ready(function(){
+  $('.datepicker').datepicker();
+});*/
+var diaSemana = ['Domingo ', 'Segunda-Feira ', 'Terca-Feira ', 'Quarta-Feira ', 'Quinta-Feira ', 'Sexta-Feira ', 'Sabado'];
+var mesAno = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'];
+var data = new Date();
+var hoje = diaSemana[data.getDay()] + ', ' + mesAno[data.getMonth()] + ' de ' + data.getFullYear();
+$("#dataPesquisa").attr("value", hoje);
+$(".datepicker").pickadate({
+  monthsFull: mesAno,
+  monthsShort: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
+  weekdaysFull: diaSemana,
+  weekdaysShort: ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sab'],
+  weekdaysLetter: ['D', 'S', 'T', 'Q', 'Q', 'S', 'S'],
+  selectMonths: true,
+  selectYears: true,
+  clear: "Limpar",
+  format: 'ddd, d/mm/yyyy',
+  today: "Hoje",
+  close: "Fechar",
+  min: new Date(data.getFullYear() - 2, 0, 1),//primeiro parâmetro é a quantidade de anos passados
+  max: new Date(data.getFullYear() + 2, 11, 31),//primeiro parâmetro é a quantidade de anos futuros
+  closeOnSelect: true
+});
+
+$("#dataPesquisa").click(function (event) {
+  event.stopPropagation();
+  $(".datepicker").first().pickadate("picker").open();
+});
+
+//timepicker
+
+$(document).ready(function(){
+  $('.timepicker').timepicker();
+});
